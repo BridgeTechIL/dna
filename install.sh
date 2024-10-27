186fe4d72dc5c4f1c5516d59f4c9d9ea85cc9b4e
BridgeTechIL <72233155+BridgeTechIL@users.noreply.github.com>
2024-10-27 18:08:52 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install python3.8-distutils
+wget https://bootstrap.pypa.io/get-pip.py && sudo python3 get-pip.py
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo python3 -m pip install --yes requirements.txt
+
+TABSIZE="set tabsize 4"
+TABTOSPACES="set tabstospaces"
+
+# File to edit
+CONFIG_FILE="/etc/nanorc"
+
+# Check if tabsize is already set
+if ! grep -qF "$TABSIZE" "$CONFIG_FILE"; then
+    echo "$TABSIZE" | sudo tee -a "$CONFIG_FILE" > /dev/null
+    echo "Added tabsize setting to $CONFIG_FILE"
+else
+    echo "Tabsize setting already exists in $CONFIG_FILE"
+fi
+
+# Check if tabstospaces is already set
+if ! grep -qF "$TABTOSPACES" "$CONFIG_FILE"; then
+    echo "$TABTOSPACES" | sudo tee -a "$CONFIG_FILE" > /dev/null
+    echo "Added tabstospaces setting to $CONFIG_FILE"
+else
+    echo "Tabstospaces setting already exists in $CONFIG_FILE"
+fi
+
+echo "Nano configuration updated."
+
+sudo apt-get -y install bluetooth bluez bluez-tools
+
+sudo cp 99-usb-serial.rules /etc/udev/rules.d/99-usb-serial.rules
+
+sudo udevadm control --reload-rules
+
+sudo udevadm trigger
